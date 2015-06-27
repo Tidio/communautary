12a2f2cfb598b9e52b6c9edc8c4fd30b38ddc0d7
gouvirtuvv <gouvirtuvv@ssh1.300gp.ha.ovh.net>
2015-06-27 22:30:00 +0200

    wpconfig

--- wp-config.php
+++ wp-config.php
@@ -19,16 +19,16 @@
 
 // ** Réglages MySQL - Votre hébergeur doit vous fournir ces informations. ** //
 /** Nom de la base de données de WordPress. */
-define('DB_NAME', 'communautary');
+define('DB_NAME', 'gouvirtuvvpre');
 
 /** Utilisateur de la base de données MySQL. */
-define('DB_USER', 'root');
+define('DB_USER', 'gouvirtuvvpre');
 
 /** Mot de passe de la base de données MySQL. */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'uKG6dZuB5rk5');
 
 /** Adresse de l'hébergement MySQL. */
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'gouvirtuvvpre.mysql.db');
 
 /** Jeu de caractères à utiliser par la base de données lors de la création des tables. */
 define('DB_CHARSET', 'utf8');
